7f80f34f15d74511ceaad5a74dcf322783e9559c
Kim Subin (김수빈) <sb2504@naver.com>
2020-05-06 22:06:28 +0900

    [Solve] VitaAlgo
    2019년 9월 1주차 : 시공의 폭풍 속으로 (Level 1) 풀이

---
+++ Algorithm/VitaAlgo/1909-1.py
@@ -0,0 +1,12 @@
+# 시공의 폭풍 속으로 (Level 1)
+
+def choice(team, my):
+	selectable = 0
+	for i in my:
+		if i not in team:
+			selectable += 1
+	return selectable
+
+team_choice = list(map(int, input().split()))
+my_choice = list(map(int, input().split()))
+print(choice(team_choice, my_choice))
