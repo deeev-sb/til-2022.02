56a83a36a73b4d72d1b6c5e6848dfafe2803a22a
김수빈 <sb2504@naver.com>
2020-06-29 12:34:32 +0900

    [Add] 1주차 과제
    1주차 과제

---
+++ Python/README.md
@@ -0,0 +1,8 @@
+# Python Programing
+
+- 파이썬프로그래밍 (계절학기)
+- 이민나 교수님
+
+## 1주차 과제
+[text_input.py](./text_input.py)
+[student.py](./student.py)
---
+++ Python/student.py
@@ -0,0 +1,8 @@
+name = input("이름 : ")
+age = input("나이 : ")
+major = input("전공 : ")
+
+print("당신의 이름은", name, "입니다.")
+print("당신의 나이는", age, "입니다.")
+print("당신의 전공은", major, "입니다.")
+print(name, age, major)
