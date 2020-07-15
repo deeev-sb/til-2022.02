e1239a63a0ba82f238b7c054bc32fc80e14e4301
Subin Kim (김수빈) <sb2504@naver.com>
2020-07-15 17:05:47 +0900

    [Solve] 예산 (Level 1)
    Solve - Programmers 예산 (Level 1)

---
+++ Algorithm/Programmers/Level1/solution(35).py
@@ -0,0 +1,11 @@
+# 예산
+# Summer/Winter Coding(~2018)
+
+def solution(d, budget):
+    answer = 0
+    d = sorted(d)
+    for i in range(len(d)):
+        if budget >= d[i]:
+            budget -= d[i]
+            answer += 1
+    return answer
