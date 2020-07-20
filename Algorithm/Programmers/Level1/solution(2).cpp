e7ed4882fd23ab142fe910b71ac44fd022968987
Subin Kim (김수빈) <sb2504@naver.com>
2020-07-20 14:34:00 +0900

    [Solve] 문자열 내림차순으로 배치하기 (Level1)
    Solve - Programmers 문자열 내림차순으로 배치하기 (Level 1)

---
+++ Algorithm/Programmers/Level1/solution(2).cpp
@@ -0,0 +1,21 @@
+// 문자열 내림차순으로 배치하기
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    for (int i = 0; i < s.size(); i++){
+        for (int j = i+1; j <= s.size(); j++){
+            if (s[i] < s[j]){
+                char temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
+    answer = s;
+    return answer;
+}
