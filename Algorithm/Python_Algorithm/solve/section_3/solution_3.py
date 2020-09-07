b5dfb158054dd493cb1d837e2cbdcea3a5815ee4
Kim-SuBin <sb2504@naver.com>
2020-09-07 14:40:06 +0900

    [Solve] 카드 역배치 (정올 기출)
    파이썬 알고리즘 문제풀이 (코딩테스트) - 카드 역배치 (정올 기출)

--- Algorithm/Python_Algorithm/solve/section_3/solution_3.py
+++ Algorithm/Python_Algorithm/solve/section_3/solution_3.py
@@ -1,14 +1,15 @@
 # 카드 역배치 (정올 기출)
 
 def solution(a, b, num):
-    for i in range((b-a)//2):
-        num[a+i-1], num[b-i-1] = num[b-i-1], num[a+i-1]
-    else:
-        return num
+    for i in range((b-a+1)//2):
+        num[a+i], num[b-i] = num[b-i], num[a+i]
+    return num
 
-num = list(range(1,21))
+num = list(range(21))
 for i in range(10):
     a, b = map(int, input().split())
     num = solution(a, b, num)
-else:
-    print(num)
+
+
+for i in range(1, 21):
+    print(num[i], end = ' ')
