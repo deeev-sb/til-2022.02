d68097838ff76adb42aec6ee33012dc0993a30a7
Kim-SuBin <sb2504@naver.com>
2020-10-08 17:30:16 +0900

    [Solve] 두 개 뽑아서 더하기 (Level 1)
    Solve - Programmers 두 개 뽑아서 더하기 (Level 1)

---
+++ Algorithm/Programmers/Level1/solution(41).py
@@ -0,0 +1,10 @@
+# 두 개 뽑아서 더하기
+
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)-1):
+        for j in range(i+1, len(numbers)):
+            answer.append(numbers[i] + numbers[j])
+    answer = list(set(answer))
+    answer.sort()
+    return answer
