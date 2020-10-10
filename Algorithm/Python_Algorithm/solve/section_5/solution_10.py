9e76fb4f2bf0ec1512f39b356a219b93f46ba981
Kim-SuBin <sb2504@naver.com>
2020-10-10 12:56:46 +0900

    [Solve] 최소힙
    파이썬 알고리즘 문제풀이 (코딩테스트) - 최소힙

---
+++ Algorithm/Python_Algorithm/solve/section_5/solution_10.py
@@ -0,0 +1,23 @@
+# 최소힙
+
+import heapq
+
+def solution(num):
+    heap = []
+    for n in num:
+        if n == 0:
+            if heap:
+                print(heapq.heappop(heap))
+            else:
+                print(-1)
+        elif n == -1:
+            break
+        else:
+            heapq.heappush(heap, n)
+
+num = []
+while True:
+    num.append(int(input()))
+    if num[-1] == -1:
+        break
+solution(num)
