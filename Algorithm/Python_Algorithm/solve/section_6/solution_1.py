608d31add841d14c7f6be8b7eee3f4fc3b5c891e
Kim-SuBin <sb2504@naver.com>
2020-10-16 14:10:49 +0900

    [Solve] 재귀함수를 이용한 이진수 출력
    파이썬 알고리즘 문제풀이 (코딩테스트) - 재귀함수를 이용한 이진수 출력

--- Algorithm/Python_Algorithm/solve/section_6/solution_1.py
+++ Algorithm/Python_Algorithm/solve/section_6/solution_1.py
@@ -1,11 +1,22 @@
 # 재귀함수를 이용한 이진수 출력
 
-def solution(num):
-    answer = ''
-    while num:
+
+# def solution(num):
+#     answer = ''
+#     while num:
+#         answer = str(num%2) + answer
+#         num = num // 2
+#     return answer
+
+# 재귀함수를 사용하지 않아서 다시 작성
+
+def solution(answer, num):
+    if num == 0:
+        print(answer)
+    else:
         answer = str(num%2) + answer
-        num = num // 2
-    return answer
+        solution(answer, num//2)
 
 num = int(input())
-print(solution(num))
+answer = ''
+solution(answer, num)
