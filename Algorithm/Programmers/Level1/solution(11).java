61e09a3eeb9d132636d3db4783a3c3fbe7669de4
Kim-SuBin <sb2504@naver.com>
2020-12-30 00:28:27 +0900

    [Add] ✍ 가운데 글자 가져오기 (Java)
    Solve - Programmers 가운데 글자 가져오기

---
+++ Algorithm/Programmers/Level1/solution(11).java
@@ -0,0 +1,15 @@
+// 가운데 글자 가져오기
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int n = s.length();
+        if (n % 2 == 0){
+            answer += s.charAt(n/2-1);
+            answer += s.charAt(n/2);
+        }
+        else
+            answer += s.charAt(n/2);
+        return answer;
+    }
+}
