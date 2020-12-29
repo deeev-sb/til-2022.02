85e8468e113f3180b9243592f2f2840ced446dcb
Kim-SuBin <sb2504@naver.com>
2020-12-29 23:43:15 +0900

    [Add] ✍ 서울에서 김서방 찾기 (Java)
    Solve - Programmers 서울에서 김서방 찾기

---
+++ Algorithm/Programmers/Level1/solution(10).java
@@ -0,0 +1,14 @@
+// 서울에서 김서방 찾기
+
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        int location = 0;
+        for (int i = 0; i < seoul.length; i++){
+            if (seoul[i].equals("Kim"))
+                location = i;
+        }
+        answer = "김서방은 " + location + "에 있다";
+        return answer;
+    }
+}
