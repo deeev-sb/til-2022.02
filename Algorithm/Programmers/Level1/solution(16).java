e5a3241a46e5056658791604877cb80495c99eba
Kim-SuBin <sb2504@naver.com>
2021-01-06 00:31:42 +0900

    [Add] ✍ 문자열 다루기 기본 (Java)
    Solve - Programmers

---
+++ Algorithm/Programmers/Level1/solution(16).java
@@ -0,0 +1,16 @@
+// 문자열 다루기 기본
+
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if (s.length() != 4 && s.length() != 6)
+            answer = false;
+        else {
+            for (int i = 0; i < s.length(); i++){
+                if (!('0' <= s.charAt(i) && s.charAt(i) <= '9'))
+                    answer = false;
+            }
+        }
+        return answer;
+    }
+}
