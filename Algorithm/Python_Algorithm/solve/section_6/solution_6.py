f101ea97e75261354ed92d4047a14bba9b76faa8
Kim-SuBin <sb2504@naver.com>
2021-01-11 23:16:50 +0900

    [Solve] ✍ 중복순열 구하기
    Python Programi파이썬 알고리즘 문제풀이 (코딩테스트) - 중복순열 구하기

---
+++ Algorithm/Python_Algorithm/solve/section_6/solution_6.py
@@ -0,0 +1,18 @@
+def DFS(idx):
+    global cnt
+    if idx == m:
+        for i in range(m):
+            print(temp[i], end=' ')
+        print()
+        cnt += 1
+    else:
+        for i in range(1, n+1):
+            temp[idx] = i
+            DFS(idx + 1)
+
+if __name__=="__main__":
+    n, m = map(int, input().split())
+    temp = [1]*n
+    cnt = 0
+    DFS(0)
+    print(cnt)
