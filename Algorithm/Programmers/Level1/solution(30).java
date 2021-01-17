d34d40503b932b72d4689b0b16fabdcddecb4373
Kim-SuBin <sb2504@naver.com>
2021-01-17 15:49:32 +0900

    [Solve] ✍ 시저 암호
    Solve - Programmers 시저 암호

---
+++ Algorithm/Programmers/Level1/solution(30).java
@@ -0,0 +1,24 @@
+// 시저 암호
+
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+        for (int i = 0; i < s.length(); i++){
+            if ('A' <= s.charAt(i) && s.charAt(i) <= 'Z'){
+                if((s.charAt(i)+n) > 'Z')
+                    answer += (char)(s.charAt(i) + n - 26);
+                else
+                    answer += (char)(s.charAt(i)+n);
+            }
+            else if ('a' <= s.charAt(i) && s.charAt(i) <= 'z'){
+                if((s.charAt(i)+n) > 'z')
+                    answer += (char)(s.charAt(i) + n - 26);
+                else
+                    answer += (char)(s.charAt(i) + n);
+            }
+            else
+                answer += s.charAt(i);
+        }
+        return answer;
+    }
+}
